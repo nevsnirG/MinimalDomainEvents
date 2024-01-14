32ca46f018a5265c58b0d66f108d2d6c5be2aae7
Ken <ken@vgss.io>
2024-01-14 23:18:58 +0100

    Add tests

--- test/MinimalDomainEvents.Outbox.MongoDb.UnitTests/IOutboxDispatcherBuilderExtensionsTests.cs
+++ test/MinimalDomainEvents.Outbox.MongoDb.UnitTests/IOutboxDispatcherBuilderExtensionsTests.cs
@@ -1,13 +1,41 @@
+using FluentAssertions;
 using Microsoft.Extensions.DependencyInjection;
+using MinimalDomainEvents.Contract;
+using MinimalDomainEvents.Dispatcher;
+using MinimalDomainEvents.Outbox.Abstractions;
+using MongoDB.Driver;
+using MongoDB.Driver.Linq;
 
 namespace MinimalDomainEvents.Outbox.MongoDb.UnitTests;
 
-public class IOutboxDispatcherBuilderExtensionsTests
+[CollectionDefinition("MongoDb Integration")]
+public class IOutboxDispatcherBuilderExtensionsTests(MongoContainerFixture fixture)
 {
     [Fact]
-    public void Test1()
+    public async Task WithDatabase_AfterUseMongo_StillWorks()
     {
-        var serviceCollection = new ServiceCollection();
+        var mongoClient = new MongoClient(fixture.ConnectionString);
+        IServiceCollection serviceCollection = new ServiceCollection();
+        serviceCollection.AddDomainEventDispatcher(dispatcherBuilder =>
+        {
+            dispatcherBuilder.AddOutbox(outboxBuilder =>
+            {
+                outboxBuilder.AddMongo(mongoClient);
+                outboxBuilder.WithDatabase("testdatabase");
+            });
+        });
+        var serviceProvider = serviceCollection.BuildServiceProvider();
+        var persister = serviceProvider.GetRequiredService<IPersistDomainEvents>();
 
+        await persister.Persist(new List<IDomainEvent>(1)
+        {
+            new TestEvent()
+        });
+
+        var outboxCollection = mongoClient.GetDatabase("testdatabase").GetCollection<OutboxRecord>("OutboxRecords");
+        var item = await outboxCollection.AsQueryable().SingleOrDefaultAsync();
+        item.Should().NotBeNull();
     }
+
+    public sealed record TestEvent() : IDomainEvent;
 }
---
+++ test/MinimalDomainEvents.Outbox.MongoDb.UnitTests/MongoContainerFixture.cs
@@ -0,0 +1,29 @@
+﻿using Testcontainers.MongoDb;
+
+namespace MinimalDomainEvents.Outbox.MongoDb.UnitTests;
+[CollectionDefinition("MongoDb Integration")]
+public sealed class MognoContainerCollectionFixture : ICollectionFixture<MongoContainerFixture> { }
+
+public sealed class MongoContainerFixture : IAsyncLifetime
+{
+    public string ConnectionString { get; private set; } = string.Empty;
+    private readonly MongoDbContainer _container;
+
+    public MongoContainerFixture()
+    {
+        _container = new MongoDbBuilder()
+            .WithImage("mongo:6.0")
+            .Build();
+    }
+
+    public async Task InitializeAsync()
+    {
+        await _container.StartAsync();
+        ConnectionString = _container.GetConnectionString();
+    }
+
+    public async Task DisposeAsync()
+    {
+        await _container.DisposeAsync();
+    }
+}
