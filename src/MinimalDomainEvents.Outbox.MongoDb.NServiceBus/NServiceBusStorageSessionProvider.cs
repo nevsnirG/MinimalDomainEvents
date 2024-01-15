132048b0521f6850361a6a9aa320a89de52fdd23
Ken <ken@vgss.io>
2024-01-15 22:52:33 +0100

    Add NServiceBus MongoDb Outbox package

---
+++ src/MinimalDomainEvents.Outbox.MongoDb.NServiceBus/NServiceBusStorageSessionProvider.cs
@@ -0,0 +1,15 @@
+﻿using MongoDB.Driver;
+using NServiceBus.Storage.MongoDB;
+
+namespace MinimalDomainEvents.Outbox.MongoDb.NServiceBus;
+internal sealed class NServiceBusStorageSessionProvider : ITransactionProvider
+{
+    private readonly IMongoSynchronizedStorageSession _sharedSession;
+
+    public NServiceBusStorageSessionProvider(IMongoSynchronizedStorageSession sharedSession)
+    {
+        _sharedSession = sharedSession;
+    }
+
+    public IClientSessionHandle? Session => _sharedSession.MongoSession;
+}
