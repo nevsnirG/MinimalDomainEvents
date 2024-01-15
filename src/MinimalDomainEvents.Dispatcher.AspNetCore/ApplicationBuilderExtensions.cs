c8b078dd1c8ac4fe3dd952afc812c851e154e032
Ken <ken@vgss.io>
2024-01-15 22:33:39 +0100

    Rename file

---
+++ src/MinimalDomainEvents.Dispatcher.AspNetCore/ApplicationBuilderExtensions.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+namespace MinimalDomainEvents.Dispatcher.AspNetCore;
+public static class ApplicationBuilderExtensions
+{
+    public static IApplicationBuilder UseDomainEventDispatcherMiddleware(this IApplicationBuilder applicationBuilder)
+    {
+        return applicationBuilder.UseMiddleware<DomainEventDispatcherMiddleware>();
+    }
+}
--- src/MinimalDomainEvents.Dispatcher.AspNetCore/DependencyInjectionConfiguration.cs
+++
@@ -1,7 +0,0 @@
-﻿using Microsoft.AspNetCore.Builder;
-
-namespace MinimalDomainEvents.Dispatcher.AspNetCore;
-public static class DependencyInjectionConfiguration
-{
-    public static IApplicationBuilder UseDomainEventDispatcherMiddleware(this IApplicationBuilder applicationBuilder) => applicationBuilder.UseMiddleware<DomainEventDispatcherMiddleware>();
-}
