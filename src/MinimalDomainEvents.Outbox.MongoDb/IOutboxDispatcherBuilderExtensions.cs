f10591654dd71232b23f0469b1faabfcd562d3bd
Ken <ken@vgss.io>
2024-01-15 22:51:59 +0100

    Add TransactionProvider

--- src/MinimalDomainEvents.Outbox.MongoDb/IOutboxDispatcherBuilderExtensions.cs
+++ src/MinimalDomainEvents.Outbox.MongoDb/IOutboxDispatcherBuilderExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
 using MinimalDomainEvents.Outbox.Abstractions;
 using MongoDB.Bson.Serialization;
 using MongoDB.Driver;
@@ -8,16 +9,27 @@ public static class IOutboxDispatcherBuilderExtensions
 {
     public static IOutboxDispatcherBuilder AddMongo(this IOutboxDispatcherBuilder builder, MongoClient mongoClient)
     {
-        TryRegisterOutboxRecordClassMap();
+        RegisterDefaultServices(builder);
+
         builder.Services.AddScoped<IPersistOutboxRecords>(sp => ActivatorUtilities.CreateInstance<MongoDbDomainEventPersister>(sp, builder.OutboxSettings, mongoClient));
         return builder;
     }
 
     public static IOutboxDispatcherBuilder AddMongo(this IOutboxDispatcherBuilder builder, Func<IServiceProvider, MongoClient> mongoClientFactory)
     {
-        TryRegisterOutboxRecordClassMap();
+        RegisterDefaultServices(builder);
+
         builder.Services.AddScoped<IPersistOutboxRecords>(sp => ActivatorUtilities.CreateInstance<MongoDbDomainEventPersister>(sp, builder.OutboxSettings, mongoClientFactory(sp)));
         return builder;
+    }
+
+    private static void RegisterDefaultServices(IOutboxDispatcherBuilder builder)
+    {
+        TryRegisterOutboxRecordClassMap();
+
+        builder.Services.TryAddScoped<TransactionProvider>();
+        builder.Services.TryAddScoped<ITransactionProvider>(sp => sp.GetRequiredService<TransactionProvider>());
+        builder.Services.TryAddScoped<ITransactionProviderInitializer>(sp => sp.GetRequiredService<TransactionProvider>());
     }
 
     private static bool TryRegisterOutboxRecordClassMap()
---
+++ src/MinimalDomainEvents.Outbox.MongoDb/ITransactionProvider.cs
@@ -0,0 +1,24 @@
+﻿using MongoDB.Driver;
+
+namespace MinimalDomainEvents.Outbox.MongoDb;
+public interface ITransactionProvider
+{
+    IClientSessionHandle? Session { get; }
+}
+
+public interface ITransactionProviderInitializer
+{
+    void Initialize(IClientSessionHandle session);
+}
+
+internal sealed class TransactionProvider : ITransactionProvider, ITransactionProviderInitializer
+{
+    public IClientSessionHandle? Session => _session;
+
+    private IClientSessionHandle? _session;
+
+    public void Initialize(IClientSessionHandle session)
+    {
+        _session = session;
+    }
+}
