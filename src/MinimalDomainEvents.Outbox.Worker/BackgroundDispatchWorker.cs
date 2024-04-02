ff3b6a83925105a2411adba25d13fba33d5b74dc
Ken <ken@vgss.io>
2024-04-02 22:30:35 +0200

    Remove unnecessary transaction handling from dispatch worker

--- src/MinimalDomainEvents.Outbox.Worker/BackgroundDispatchWorker.cs
+++ src/MinimalDomainEvents.Outbox.Worker/BackgroundDispatchWorker.cs
@@ -7,23 +7,20 @@ using MinimalDomainEvents.Outbox.Worker.Abstractions;
 namespace MinimalDomainEvents.Outbox.Worker;
 internal sealed class BackgroundDispatchWorker : BackgroundService
 {
-    private const int Delay = 2 * 1000;
+    private const int Delay = 1000;
 
     private readonly IOutboxRecordCollectionInitializer _outboxRecordCollectionInitializer;
-    private readonly ITransactionProvider _transactionFactory;
     private readonly IDomainEventRetriever _domainEventRetriever;
     private readonly ICleanupOutboxRecords _cleanupOutboxRecords;
     private readonly IEnumerable<IDispatchDomainEvents> _dispatchers;
 
     public BackgroundDispatchWorker(IOutboxRecordCollectionInitializer outboxRecordCollectionInitializer,
-                                    ITransactionProvider transactionFactory,
                                     IDomainEventRetriever domainEventRetriever,
                                     ICleanupOutboxRecords cleanupOutboxRecords,
                                     IEnumerable<IDispatchDomainEvents> dispatchers)
     {
         _domainEventRetriever = domainEventRetriever;
         _outboxRecordCollectionInitializer = outboxRecordCollectionInitializer;
-        _transactionFactory = transactionFactory;
         _cleanupOutboxRecords = cleanupOutboxRecords;
         _dispatchers = dispatchers;
     }
@@ -34,7 +31,6 @@ internal sealed class BackgroundDispatchWorker : BackgroundService
 
         while (!stoppingToken.IsCancellationRequested)
         {
-            using var transaction = await _transactionFactory.NewTransaction(stoppingToken);
             var domainEvents = await _domainEventRetriever.GetAndMarkAsDispatched(stoppingToken);
 
             if (domainEvents is not null && domainEvents.Count > 0)
@@ -44,7 +40,6 @@ internal sealed class BackgroundDispatchWorker : BackgroundService
             }
 
             await _cleanupOutboxRecords.CleanupExpiredOutboxRecords(stoppingToken);
-            await transaction.Commit(stoppingToken);
 
             await Task.Delay(Delay, stoppingToken);
         }
