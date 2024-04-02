fe163ce44522fa456376a78bda1e7aacff46ec11
Ken <ken@vgss.io>
2024-04-02 22:31:12 +0200

    Re-add transaction handling in dispatch worker as it was needed

--- src/MinimalDomainEvents.Outbox.Worker/BackgroundDispatchWorker.cs
+++ src/MinimalDomainEvents.Outbox.Worker/BackgroundDispatchWorker.cs
@@ -10,17 +10,20 @@ internal sealed class BackgroundDispatchWorker : BackgroundService
     private const int Delay = 1000;
 
     private readonly IOutboxRecordCollectionInitializer _outboxRecordCollectionInitializer;
+    private readonly ITransactionProvider _transactionFactory;
     private readonly IDomainEventRetriever _domainEventRetriever;
     private readonly ICleanupOutboxRecords _cleanupOutboxRecords;
     private readonly IEnumerable<IDispatchDomainEvents> _dispatchers;
 
     public BackgroundDispatchWorker(IOutboxRecordCollectionInitializer outboxRecordCollectionInitializer,
+                                    ITransactionProvider transactionFactory,
                                     IDomainEventRetriever domainEventRetriever,
                                     ICleanupOutboxRecords cleanupOutboxRecords,
                                     IEnumerable<IDispatchDomainEvents> dispatchers)
     {
         _domainEventRetriever = domainEventRetriever;
         _outboxRecordCollectionInitializer = outboxRecordCollectionInitializer;
+        _transactionFactory = transactionFactory;
         _cleanupOutboxRecords = cleanupOutboxRecords;
         _dispatchers = dispatchers;
     }
@@ -31,6 +34,7 @@ internal sealed class BackgroundDispatchWorker : BackgroundService
 
         while (!stoppingToken.IsCancellationRequested)
         {
+            using var transaction = await _transactionFactory.NewTransaction(stoppingToken);
             var domainEvents = await _domainEventRetriever.GetAndMarkAsDispatched(stoppingToken);
 
             if (domainEvents is not null && domainEvents.Count > 0)
@@ -40,6 +44,7 @@ internal sealed class BackgroundDispatchWorker : BackgroundService
             }
 
             await _cleanupOutboxRecords.CleanupExpiredOutboxRecords(stoppingToken);
+            await transaction.Commit(stoppingToken);
 
             await Task.Delay(Delay, stoppingToken);
         }
